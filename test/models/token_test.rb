939fe50922e71723d8263b9af9433a1b29fb560d
joshua-anderson <j@zatigo.com>
2014-09-30 20:55:24 -0700

    Add test for Token class

---
+++ test/models/token_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class TokenTest < ActiveSupport::TestCase
+  test "Generates, uses, and deletes token correctly" do
+    test_user_id = "0"
+    token = Token.generate(test_user_id)
+
+    assert_equal Token.authenticate(token), test_user_id
+
+    Token.delete(token)
+
+    assert_equal Token.authenticate(token), nil
+  end
+end
