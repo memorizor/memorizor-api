a39cbb31081b492eb40873cd69fffeddbd043326
Joshua Anderson <j@joshua-anderson.com>
2014-10-02 19:13:16 -0700

    Convert tabs to spaces in verification token test

--- test/models/verification_token_test.rb
+++ test/models/verification_token_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class VerificationTokenTest < ActiveSupport::TestCase
-	test "Generates, uses, and deletes verification token correctly" do
-		test_user_id = users(:verification_token).id
-		token = VerificationToken.generate test_user_id
-		VerificationToken.verify token
+  test "Generates, uses, and deletes verification token correctly" do
+    test_user_id = users(:verification_token).id
+    token = VerificationToken.generate test_user_id
+    VerificationToken.verify token
 
-		assert_equal User.find_by_id(test_user_id).verified, true
-		assert_not $redis.exists("verify." << token)
-	end
+    assert_equal User.find_by_id(test_user_id).verified, true
+    assert_not $redis.exists("verify." << token)
+  end
 end
